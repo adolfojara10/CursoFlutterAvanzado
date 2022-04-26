2e8a42846390301885c484792a3a6d25c6c3b8a3
adolfojara10 <gavilanesadolfo@gmail.com>
2022-04-25 20:14:29 -0500

    Firebase

--- Curso-de-Flutter-Avanzado-en-Platzi-master/lib/platzi_trips.dart
+++ Curso-de-Flutter-Avanzado-en-Platzi-master/lib/platzi_trips.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'place/ui/screens/home_trips.dart';
 import 'place/ui/screens/search_trips.dart';
-import 'profile_trips.dart';
+import 'package:platzi_trips_app/user/ui/screens/profile_trips.dart';
 
 class PlatziTrips extends StatefulWidget {
   @override
--- Curso-de-Flutter-Avanzado-en-Platzi-master/lib/platzi_trips_cupertino.dart
+++ Curso-de-Flutter-Avanzado-en-Platzi-master/lib/platzi_trips_cupertino.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'place/ui/screens/home_trips.dart';
 import 'place/ui/screens/search_trips.dart';
-import 'profile_trips.dart';
+import 'package:platzi_trips_app/user/ui/screens/profile_trips.dart';
 
 class PlatziTripsCupertino extends StatelessWidget {
   @override
@@ -10,23 +10,14 @@ class PlatziTripsCupertino extends StatelessWidget {
     // TODO: implement build
     return Scaffold(
       bottomNavigationBar: CupertinoTabScaffold(
-        tabBar: CupertinoTabBar(
-            items: [
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.home, color: Colors.indigo),
-                  label: ""
-              ),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.search, color: Colors.indigo),
-                  label: ""
-              ),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.person, color: Colors.indigo),
-                  label: ""
-              ),
-            ]
-        ),
-
+        tabBar: CupertinoTabBar(items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: Colors.indigo), label: ""),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search, color: Colors.indigo), label: ""),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person, color: Colors.indigo), label: ""),
+        ]),
         tabBuilder: (BuildContext context, int index) {
           switch (index) {
             case 0:
@@ -44,12 +35,9 @@ class PlatziTripsCupertino extends StatelessWidget {
                 builder: (BuildContext context) => ProfileTrips(),
               );
               break;
-
           }
-
         },
       ),
     );
   }
-
 }
